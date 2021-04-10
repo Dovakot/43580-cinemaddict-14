ca1cf3623f25afccab20f91c3c6dce3266c3c96e
Anastasiya Yakovleva <dovakot@yandex.ru>
2021-04-11 00:11:46 +0800

    Добавляет вспомогательные функции

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,9 @@
+import {
+  MAX_HOURS
+} from 'const';
+
+const DATE_OFFSET = 60 * 60 * 1000;
+
 const Position = {
   AFTERBEGIN: 'afterbegin',
   BEFOREEND: 'beforeend',
@@ -25,9 +31,130 @@ const render = (container, element, place = Position.BEFOREEND) => {
   }
 };
 
+const getRandomNumber = (min, max, maxInc = 0) => {
+  const diff = max - min;
+
+  if (diff < 0) {
+    throw new Error(`Значение max(${max}) не должно быть меньше min(${min})`);
+  }
+
+  return (diff === 0) ? min : Math.random() * (diff + maxInc) + min;
+};
+
+const getRandomInt = (min, max) => {
+  if (!Number.isInteger(min) || !Number.isInteger(max)) {
+    throw new Error(`Значения min(${min}) и max(${max}) должны быть целочисленными`);
+  }
+
+  const randomNumber = getRandomNumber(min, max, 1);
+
+  return Math.floor(randomNumber);
+};
+
+const getRandomFloat = (min, max, digits = 1) => {
+  const randomNumber = getRandomNumber(min, max);
+  const numberRank = 10 ** digits;
+
+  return Math.floor(randomNumber * numberRank) / numberRank;
+};
+
+const getRandomArrayIndex = (length) => {
+  if (length < 0) {
+    throw new Error(`Значение length(${length}) не должно быть отрицательным`);
+  }
+
+  const newArray = [];
+
+  for (let counter = 0; counter < length; counter++) {
+    const index = getRandomInt(0, length - 1);
+
+    newArray.push(index);
+  }
+
+  return newArray;
+};
+
+const getRandomArrayElement = (array) => {
+  const length = array.length - 1;
+  const index = (length >= 0) ? getRandomInt(0, length) : 0;
+
+  return array[index];
+};
+
+const getRandomArray = (array, amount = array.length) => {
+  const length = array.length;
+
+  if (amount > length || amount < 0) {
+    throw new Error(`Значение amount(${amount}) не должно быть отрицательным и больше array.length(${length})`);
+  }
+
+  const newArray = [];
+
+  for (let counter = 0; counter < amount; counter++) {
+    const item = getRandomArrayElement(array);
+
+    newArray.push(item);
+  }
+
+  return newArray;
+};
+
+const getRandomObjects = (generator, amount) => [...Array(amount)].map(() => generator());
+
+const getRandomDate = (days, hours = MAX_HOURS) => {
+  const amountDays = DATE_OFFSET * hours * days;
+
+  return Date.now() + amountDays;
+};
+
+const getFormattedDate = (date) => {
+  return new Date(date).toLocaleDateString('en-GB', {
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric',
+  }).split(' ').join(' ');
+};
+
+const getFormattedDateTime = (date) => {
+  const formattedDate = new Date(date).toLocaleDateString('fr-CA', {
+    year: '2-digit',
+    month: 'numeric',
+    day: 'numeric',
+  }).split('-').join('/');
+
+  const formattedTime = new Date(date).toLocaleTimeString('en-GB', {
+    hour: 'numeric',
+    minute: 'numeric',
+  }).split(' ').join(':');
+
+  return `${formattedDate} ${formattedTime}`;
+};
+
+const getTimeFromMinutes = (amount) => {
+  const hours = Math.trunc(amount / 60);
+  const minutes = amount % 60;
+
+  return `${hours}h ${minutes}m`;
+};
+
+const truncateText = (text, amount) => {
+  return text.length > amount ? `${text.substring(0, amount)}...` : text;
+};
+
 export {
   Position,
   getElement,
   render,
-  replaceElement
+  replaceElement,
+  getRandomArrayIndex,
+  getRandomArrayElement,
+  getRandomArray,
+  getRandomInt,
+  getRandomFloat,
+  getRandomDate,
+  getFormattedDate,
+  getFormattedDateTime,
+  getTimeFromMinutes,
+  getRandomObjects,
+  truncateText
 };
