59782241322e8b147fd0d5ac102c070dda750888
Anastasiya Yakovleva <dovakot@yandex.ru>
2021-05-05 22:09:38 +0800

    Реализует подписку на изменение сортировки

--- src/view/sort.js
+++ src/view/sort.js
@@ -1,16 +1,41 @@
 import AbstractView from './abstract';
 
-const createSortTemplate = () => (
+import {
+  SortType
+} from 'const';
+
+const isActive = (flag) => flag ? 'sort__button--active' : '';
+
+const createSortTemplate = (currentSort) => (
   `<ul class="sort">
-    <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-    <li><a href="#" class="sort__button">Sort by date</a></li>
-    <li><a href="#" class="sort__button">Sort by rating</a></li>
+    <li><a href="#" class="sort__button ${isActive(currentSort === SortType.DEFAULT)}" data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
+    <li><a href="#" class="sort__button ${isActive(currentSort === SortType.DATE)}" data-sort-type="${SortType.DATE}">Sort by date</a></li>
+    <li><a href="#" class="sort__button ${isActive(currentSort ===SortType.RATING)}" data-sort-type="${SortType.RATING}">Sort by rating</a></li>
   </ul>`
 );
 
 class Sort extends AbstractView {
+  constructor(currentSort) {
+    super();
+    this._currentSort = currentSort;
+
+    this._sortTypeChangeHandler = this._sortTypeChangeHandler.bind(this);
+  }
+
   getTemplate() {
-    return createSortTemplate();
+    return createSortTemplate(this._currentSort);
+  }
+
+  _sortTypeChangeHandler(evt) {
+    if (!evt.target.classList.contains('sort__button')) return;
+
+    evt.preventDefault();
+    this._callback.sortTypeChange(evt.target.dataset.sortType);
+  }
+
+  setSortTypeChangeHandler(callback) {
+    this._callback.sortTypeChange = callback;
+    this.getElement().addEventListener('click', this._sortTypeChangeHandler);
   }
 }
 
