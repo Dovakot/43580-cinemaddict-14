cdaf1447c6e9739c55390a4eb0c76f4be548cffb
Anastasiya Yakovleva <dovakot@yandex.ru>
2021-05-20 03:31:08 +0800

    Выносит часть логики из точки входа в презентер

--- src/main.js
+++ src/main.js
@@ -2,57 +2,35 @@
 /*eslint-env node*/
 
 import {
-  AppConfig,
-  RenderPosition
+  AppConfig
 } from 'const';
 
 import {
   getRandomObjects
 } from 'utils/common-util';
 
-import {
-  render
-} from 'utils/render-util';
-
 import FilmsModel from 'model/films-model';
 import CommentsModel from 'model/comments-model';
 
-import FilmsPresenter from 'presenter/films-presenter';
-
-import UserLevelView from 'view/user-level-view';
-import MenuView from 'view/menu-view';
-import FilterView from 'view/filter-view';
-import FooterStatisticsView from 'view/footer-statistics-view';
-
 import generateFilm from 'mock/film-card-mock';
 import generateComment from 'mock/comment-mock';
-import {
-  generateFilter
-} from 'mock/filter-mock';
+
+import BoardPresenter from 'presenter/board-presenter';
 
 const containerHeader = document.querySelector('.header');
 const containerMain = document.querySelector('.main');
 const containerFooter = document.querySelector('.footer');
 
 const films = getRandomObjects(generateFilm, AppConfig.MAX_FILMS);
-const comments = getRandomObjects(generateComment, AppConfig.MAX_COMMENTS);
-const filters = generateFilter(films);
+const comments = getRandomObjects(() => generateComment(), AppConfig.MAX_COMMENTS);
 
 const filmsModel = new FilmsModel();
 const commentsModel = new CommentsModel();
 
-const filterComponent = new FilterView(filters).getTemplate();
-
-const filmsPresenter = new FilmsPresenter(containerMain, filmsModel, commentsModel);
 filmsModel.init(films);
 commentsModel.init(comments);
-filmsPresenter.init();
 
-render(containerMain, new MenuView(filterComponent), RenderPosition.AFTERBEGIN);
-render(containerFooter, new FooterStatisticsView(filmsModel.length));
-
-if (filmsModel.length) {
-  const [, userHistory] = filters;
-
-  render(containerHeader, new UserLevelView(userHistory));
-}
+const boardPresenter = new BoardPresenter(
+  containerHeader, containerMain, containerFooter, filmsModel, commentsModel,
+);
+boardPresenter.init();
---
+++ src/presenter/board-presenter.js
@@ -0,0 +1,63 @@
+import {
+  render,
+  remove
+} from 'utils/render-util';
+
+import MenuModel from 'model/menu-model';
+
+import UserLevelView from 'view/user-level-view';
+import FooterStatisticsView from 'view/footer-statistics-view';
+
+import FilmsPresenter from 'presenter/films-presenter';
+import MenuPresenter from 'presenter/menu-presenter';
+
+class BoardPresenter {
+  constructor(containerHeader, containerMain, containerFooter, filmsModel, commentsModel) {
+    this._containerHeader = containerHeader;
+    this._containerMain = containerMain;
+    this._containerFooter = containerFooter;
+
+    this._menuModel = new MenuModel();
+    this._filmsModel = filmsModel;
+    this._commentsModel = commentsModel;
+    this._userLevelComponent = null;
+    this._menuPresenter = null;
+    this._filmsPresenter = null;
+
+    this._updateUserLevel = this._updateUserLevel.bind(this);
+  }
+
+  init() {
+    this._initPresenters();
+    this._renderFooterStatistics();
+
+    if (this._filmsModel.length) {
+      this._updateUserLevel();
+    }
+  }
+
+  _initPresenters() {
+    this._filmsPresenter = new FilmsPresenter(this._containerMain, this._filmsModel, this._commentsModel, this._menuModel);
+    this._menuPresenter = new MenuPresenter(this._containerMain, this._filmsModel, this._menuModel, this._updateUserLevel);
+
+    this._filmsPresenter.init();
+    this._menuPresenter.init();
+  }
+
+  _renderUserLevel(filmCount) {
+    remove(this._userLevelComponent);
+
+    this._userLevelComponent = new UserLevelView(filmCount);
+    render(this._containerHeader, this._userLevelComponent);
+  }
+
+  _renderFooterStatistics() {
+    render(this._containerFooter, new FooterStatisticsView(this._filmsModel.length));
+  }
+
+  _updateUserLevel(oldCount, newCount) {
+    if (oldCount !== newCount) this._renderUserLevel(newCount);
+  }
+}
+
+export default BoardPresenter;
