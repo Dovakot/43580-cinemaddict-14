f6416d1fdbd105905804a2da2b98a24cdbf3d732
Anastasiya Yakovleva <dovakot@yandex.ru>
2021-05-20 03:27:26 +0800

    Добавляет презентер и модель меню, обновляет вьюху

---
+++ src/model/menu-model.js
@@ -0,0 +1,43 @@
+import {
+  FilterType
+} from 'const';
+
+import AbstractModel from './abstract-model';
+
+class MenuModel extends AbstractModel {
+  constructor() {
+    super();
+    this._menu = {
+      currentFilter: FilterType.ALL,
+      isStatsActive: false,
+    };
+  }
+
+  setMenu(updateType, currentType) {
+    this._menu.currentFilter = currentType;
+
+    if (FilterType[currentType.toUpperCase()]) {
+      this._menu.isStatsActive = false;
+    } else {
+      this._menu.isStatsActive = true;
+    }
+
+    this._notify(updateType, this._menu, this._menu.isStatsActive);
+  }
+
+  get menu() {
+    return this._menu;
+  }
+
+  get filter() {
+    return this._menu.currentFilter;
+  }
+
+  getFilteredFilmCount(films, filters) {
+    const filteredFilmCount = filters.map((filter) => filter(films).length);
+
+    return filteredFilmCount;
+  }
+}
+
+export default MenuModel;
